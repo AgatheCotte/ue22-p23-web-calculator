3a5213550c30fb4b168e6dc6e2e49046c419deae
Agathe Cotte <agathecotte42@gmail.com>
2024-03-05 17:02:05 +0100

    avancement_cours_05/03

--- subject/script.js
+++ subject/script.js
@@ -1,5 +1,31 @@
-// NOTE: 
-// This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
+window.addEventListener("load", () => {
+    let bouttons=document.querySelectorAll("button")
+    for (let boutton of bouttons) {
+        boutton.addEventListener("click",(evenement) => {
+        const bouton = evenement.target
+        const key = evenement.target
+        const action = key.dataset.action
+        if (!action) {
+            console.log(bouton.textContent)
+        }// détection des clicks
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+          ) {
+            console.log('operator key!')
+          }
+        
+    });
+    }
+    
+})
+// les choses à faire : 
+//repérer un click -> OK
+// afficher ce qui est cliqué
+//vider l'écran Display quand on clique sur AC
+//commencer par le plan du code, qu'est-ce qu'on va faire
 
-// # START EDITING YOUR JAVASCRIPT HERE
-// ===============
+
+
