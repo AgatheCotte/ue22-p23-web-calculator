cdf4034b5ded02f53f8ec73c07ed0b7606e06226
Agathe Cotte <agathecotte42@gmail.com>
2024-03-14 13:12:32 +0100

    affichage

--- subject/script.js
+++ subject/script.js
@@ -1,13 +1,19 @@
 window.addEventListener("load", () => {
+    const display = document.querySelector('.calculator__display')
+    const calculator = document.querySelector('.calculator')
     let bouttons=document.querySelectorAll("button")
-    for (let boutton of bouttons) {
+    for (let boutton of bouttons) {//récupérer le bouton cliqué
         boutton.addEventListener("click",(evenement) => {
         const bouton = evenement.target
         const key = evenement.target
+        const keyContent = bouton.textContent
         const action = key.dataset.action
+        const displayedNum = display.textContent
+
+        // détection des clicks dans la console
         if (!action) {
-            console.log(bouton.textContent)
-        }// détection des clicks
+            console.log(keyContent)
+        }
         if (
             action === 'add' ||
             action === 'subtract' ||
@@ -16,6 +22,58 @@ window.addEventListener("load", () => {
           ) {
             console.log('operator key!')
           }
+          if (action === 'decimal') {
+            console.log('decimal key!')
+          }
+          
+          if (action === 'clear') {
+            console.log('clear key!')
+          }
+          
+          if (action === 'calculate') {
+            console.log('equal key!')
+          }
+
+          //affichage
+          //afichage d'un chiffre
+          if (!action) {
+            if (displayedNum === '0') {
+              display.textContent = keyContent
+            }
+          }
+          //afichage de plusieurs chiffres à la suite
+          if (!action) {
+            if (displayedNum === '0') {
+              display.textContent = keyContent
+            } else {
+              display.textContent = displayedNum + keyContent
+            }
+          }
+          //affichage du point
+          if (action === 'decimal') {
+            display.textContent = displayedNum + '.'
+          }
+
+          //gestion opérateurs
+          if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+          ) {
+            // Add custom attribute
+            calculator.dataset.previousKeyType = 'operator'
+          }
+          const previousKeyType = calculator.dataset.previousKeyType
+          if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+              display.textContent = keyContent
+            } else {
+              display.textContent = displayedNum + keyContent
+            }
+          }
+         
+          
         
     });
     }
@@ -23,7 +81,7 @@ window.addEventListener("load", () => {
 })
 // les choses à faire : 
 //repérer un click -> OK
-// afficher ce qui est cliqué
+// afficher ce qui est cliqué -> OK
 //vider l'écran Display quand on clique sur AC
 //commencer par le plan du code, qu'est-ce qu'on va faire
 
